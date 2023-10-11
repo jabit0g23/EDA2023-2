e8d19d12c3b792666d2bc85bc19e9e610e70e667
jabit0g23 <jabit0g23@gmail.com>
2023-10-10 22:51:03 -0300

    Nuevos archivos

---
+++ EjercicioAy3.java
@@ -0,0 +1,26 @@
+import java.util.Arrays;
+
+public class Ejercicio3S {
+
+    // Clase para combinar nombre y altura.
+    static class Person {
+    }
+
+    public static void main(String[] args) {
+        String[] names = { "Mary", "John", "Emma", "Jose" };
+        int[] heights = { 180, 165, 170, 169 };
+
+        String[] SortResult = SortPersonFunction(names, heights);
+        System.out.println("Nombres: " + Arrays.toString(SortResult));
+    }
+
+    public static String[] SortPersonFunction(String[] names, int[] heights) {
+        int n = names.length;
+
+        // Paso 1: Crear una matriz de objetos Person para combinar nombres y alturas.
+
+        // Paso 2: Algoritmo de Ordenamiento
+
+        // Paso 3: Extraer los nombres ordenados en ese orden.
+    }
+}
